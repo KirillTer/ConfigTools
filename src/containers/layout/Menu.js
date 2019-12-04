bfe873fec92d4e92f510566d351bd02bd3355670
Mark Taylor <Mark.Taylor@playtech.com>
2019-12-04 16:38:57 +0000

    #0 Layout folder rename

---
+++ src/containers/layout/Menu.js
@@ -0,0 +1,123 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import { Menu, Icon, Accordion } from "semantic-ui-react";
+import { categories } from '../../helpers/categories'
+
+const SideMenu = () => {
+
+    const [activeItemIndex, setActiveItemIndex] = useState([]);
+    const [activeCategoryIndex, setActiveCategoryIndex] = useState([]);
+
+    const handleItemClick = (e, titleProps) => {
+        console.log('!!!', titleProps)
+        const { index } = titleProps;
+        const newIndex = activeItemIndex;
+        const currentIndexPosition = activeItemIndex.indexOf(index);
+        if (currentIndexPosition > -1) {
+            newIndex.splice(currentIndexPosition, 1);
+        } else {
+            newIndex.push(index);
+        }
+        setActiveItemIndex([...newIndex]);
+    }
+
+    const handleCategoryClick = (e, titleProps) => {
+        const { index } = titleProps;
+        const newIndex = activeCategoryIndex;
+        const currentIndexPosition = activeCategoryIndex.indexOf(index);
+        if (currentIndexPosition > -1) {
+            newIndex.splice(currentIndexPosition, 1);
+        } else {
+            newIndex.push(index);
+        }
+        setActiveCategoryIndex([...newIndex]);
+    }
+
+    return (
+        <Accordion style={{ marginBottom: '2rem' }}>
+            <Accordion.Title
+                active={activeItemIndex === 0}
+                index={0}
+                onClick={handleItemClick}
+                style={{ marginTop: '1rem', padding: 0 }}
+            >
+                <Menu.Item as={Link} to='/main/Home'>
+                    <Icon name='home' style={{ float: 'left', margin: '0 1rem 0 2rem' }} />
+                    <span>Home</span>
+                </Menu.Item>
+            </Accordion.Title>
+            {categories.map((itemMenu) => {
+                return (
+                    <div key={itemMenu.id}>
+                        <Accordion.Title
+                            active={activeItemIndex.includes(itemMenu.id)}
+                            index={itemMenu.id}
+                            onClick={handleItemClick}
+                            style={{ padding: 0 }}
+                        >
+                            <Menu.Item as={Link} to={`/main/${itemMenu.title}`}>
+                                <Icon name={itemMenu.icon} style={{ float: 'left', margin: '0 1rem 0 2rem' }} />
+                                <span style={{ fontSize: '1rem', fontWeight: 'bold' }}>{itemMenu.title}</span>
+                            </Menu.Item>
+                        </Accordion.Title>
+                        <Accordion.Content active={activeItemIndex.includes(itemMenu.id)} style={{ padding: 0 }}>
+                            {itemMenu.items.map((category) => {
+                                return (category.category ?
+                                    <div key={category.id}>
+                                        <Accordion.Title
+                                            active={activeCategoryIndex.includes(category.id)}
+                                            index={category.id}
+                                            onClick={handleCategoryClick}
+                                            style={{ margin: '0.5rem 0 0.5rem 4rem', padding: 0 }}
+                                        >
+                                            <Icon name='dropdown' />
+                                            <span>{category.category}</span>
+                                        </Accordion.Title>
+                                        <Accordion.Content active={activeCategoryIndex.includes(category.id)} style={{ margin: '0 0 0 0', padding: 0 }}>
+                                            {category.elem.map((el) => {
+                                                return (
+                                                    <div key={el.id}>
+                                                        <Accordion.Title
+                                                            index={el.id}
+                                                            as={Link} to={`/main/${el.name}`}
+                                                            style={{ margin: '0.5rem 0 0.5rem 5rem', padding: 0 }}
+                                                        >
+                                                            <Icon name={el.icon} />
+                                                            <span>{el.name}</span>
+                                                        </Accordion.Title>
+                                                    </div>
+                                                )
+                                            })
+                                            }
+                                        </Accordion.Content>
+                                    </div>
+                                    :
+                                    <div key={category.id}>
+                                        {category.elem.map((el) => {
+                                            return (
+                                                <div key={el.id}>
+                                                    <Accordion.Title
+                                                        index={el.id}
+                                                        as={Link} to={`/main/${el.name}`}
+                                                        style={{ margin: '0.5rem 0 0.5rem 5rem', padding: 0 }}
+                                                    >
+                                                        <Icon name={el.icon} />
+                                                        <span>{el.name}</span>
+                                                    </Accordion.Title>
+                                                </div>
+                                            )
+                                        })
+                                        }
+                                    </div>
+                                )
+                            })
+                            }
+                        </Accordion.Content>
+                    </div>
+                )
+            })}
+        </Accordion>
+    )
+}
+
+export default SideMenu
---
+++ src/containers/layout/Search.js
@@ -0,0 +1,87 @@
+import _ from 'lodash'
+import React, { Component } from 'react'
+import { history } from '../../store/configureStore'
+import { Search, Label } from 'semantic-ui-react'
+import { categories } from '../../helpers/categories'
+
+const initialState = { isLoading: false, results: [], value: '' }
+
+const getResults = items => items.map(item => ({
+  title: item.name,
+  description: item.name
+}))
+
+const source = categories.reduce((memo, item) => {
+  const name = item.title
+  memo[name] = {
+    name,
+    results: getResults((item.items.map(i => i.elem)).flat()),
+  }
+  return memo
+}, {})
+
+export default class SearchExampleCategory extends Component {
+  state = initialState
+
+  handleResultSelect = (e, { result }) => {
+    if (this.props.val === 'shortcut') {
+      this.setState({ value: result.title }, () => {
+        this.props.parentCallback(this.state.value)
+      })
+    } else {
+      this.setState({ value: '' })
+      history.push(`/main/${result.description}`)
+    }
+  }
+
+  resultRenderer = ({ title }) => <Label content={title} />
+
+  handleSearchChange = (e, { value }) => {
+    this.setState({ isLoading: true, value })
+
+    setTimeout(() => {
+      if (this.state.value.length < 1) return this.setState(initialState)
+
+      const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+      const isMatch = (result) => re.test(result.title)
+
+      const filteredResults = _.reduce(
+        source,
+        (memo, data, name) => {
+          const results = _.filter(data.results, isMatch)
+          if (results.length) memo[name] = { name, results } // eslint-disable-line no-param-reassign
+
+          return memo
+        },
+        {},
+      )
+
+      this.setState({
+        isLoading: false,
+        results: filteredResults,
+      })
+    }, 300)
+  }
+
+  render() {
+    const { isLoading, value, results } = this.state
+
+    return (
+      <Search
+        category
+        loading={isLoading}
+        onResultSelect={this.handleResultSelect}
+        onSearchChange={_.debounce(this.handleSearchChange, 500, {
+          leading: true,
+        })}
+        results={results}
+        value={value}
+        resultRenderer={this.resultRenderer}
+        placeholder='Search Tools'
+        input={{ fluid: true }}
+        aligned='right'
+        minCharacters={3}
+      />
+    )
+  }
+}
